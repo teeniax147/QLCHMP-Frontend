5efbccd531cdab24fc5c5fceae54cd0b6facd1da
zianhng <143838447+zianhng@users.noreply.github.com>
2025-02-18 23:49:12 +0700

    update ngay 18/02 05

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -104,7 +104,7 @@ const Layout = () => {
 
         <div className="header-main">
           <Link to="/" className="logoheader">
-            <img src="/Logo.png" alt="Glamour Cosmic Logo" />
+            <img src="/imgs/Logo.png" alt="Glamour Cosmic Logo" />
           </Link>
 
           {/* Thanh tìm kiếm */}
